ShabbyX/vktut

commit 5f71874640a3d894229417946543cda4d971cb39

Cleanup improvements
Signed-off-by: Shahbaz Youssefi <ShabbyX@gmail.com>

tut6/main.c
@@ -108,21 +108,21 @@ static void render_loop(uint32_t dev_count, struct tut1_physical_device *phy_dev
 			if (res)
 			{
 				tut1_error_printf(&retval, "Failed to determine whether queue family index %u on device %u supports presentation\n", j, i);
-				return;
+				goto exit_fail;
 			}
 
 			/* Just present to the first queue of the first family we find, for now. */
 			if (supports)
 			{
 				present_queue[i] = devs[i].command_pools[j].queues[0];
-				break;
+				goto exit_fail;
 			}
 		}
 
 		if (present_queue[i] == NULL)
 		{
 			printf("Failed to find any family queue on device %u that supports presentation\n", i);
-			return;
+			goto exit_fail;
 		}
 	}
 
@@ -176,7 +176,7 @@ static void render_loop(uint32_t dev_count, struct tut1_physical_device *phy_dev
 			if (res == VK_TIMEOUT)
 			{
 				printf("A whole second and no image.  I give up.\n");
-				return;
+				goto exit_fail;
 			}
 			else if (res == VK_SUBOPTIMAL_KHR)
 				printf("Did you change the window size?  I didn't recreate the swapchains,\n"
@@ -184,7 +184,7 @@ static void render_loop(uint32_t dev_count, struct tut1_physical_device *phy_dev
 			else if (res < 0)
 			{
 				tut1_error_printf(&retval, "Couldn't acquire image\n");
-				return;
+				goto exit_fail;
 			}
 
 			/*
@@ -224,7 +224,7 @@ static void render_loop(uint32_t dev_count, struct tut1_physical_device *phy_dev
 			if (res < 0)
 			{
 				tut1_error_printf(&retval, "Failed to queue image for presentation on device %u\n", i);
-				return;
+				goto exit_fail;
 			}
 		}
 
@@ -240,6 +240,7 @@ static void render_loop(uint32_t dev_count, struct tut1_physical_device *phy_dev
 		usleep(10000);
 	}
 
+exit_fail:
 	for (uint32_t i = 0; i < dev_count; ++i)
 		free(images[i]);
 }

tut10/main.c
@@ -450,7 +450,8 @@ static void free_render_data(struct tut2_device *dev, struct tut7_render_essenti
 
 	tut8_free_pipelines(dev, &render_data->pipeline, 1);
 	tut8_free_layouts(dev, &render_data->layout, 1);
-	tut7_free_buffers(dev, render_data->buffers, 2);	/* Note: BUFFER_VERTICES_STAGING is already freed */
+	tut7_free_images(dev, render_data->images, 2);
+	tut7_free_buffers(dev, render_data->buffers, 3);
 	tut7_free_shaders(dev, render_data->shaders, 2);
 	tut7_free_graphics_buffers(dev, render_data->gbuffers, essentials->image_count, render_data->render_pass);
 
@@ -546,7 +547,7 @@ static void render_loop(struct tut1_physical_device *phy_dev, struct tut2_device
 		/* We saw all this in Tutorials 8 and 9.  Any changes are commented. */
 		res = tut7_render_start(&essentials, dev, swapchain, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL, &image_index);
 		if (res)
-			return;
+			break;
 
 		/* Render pass */
 		VkClearValue clear_values[2] = {
@@ -623,7 +624,7 @@ static void render_loop(struct tut1_physical_device *phy_dev, struct tut2_device
 
 		res = tut7_render_finish(&essentials, dev, swapchain, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL, image_index);
 		if (res)
-			return;
+			break;
 	}
 
 exit_bad_render_data:

tut7/main.c
@@ -162,7 +162,7 @@ static void render_loop(uint32_t dev_count, struct tut1_physical_device *phy_dev
 			if (res)
 			{
 				printf("-- failed for device %u\n", i);
-				return;
+				goto exit_fail;
 			}
 
 			/*
@@ -208,11 +208,12 @@ static void render_loop(uint32_t dev_count, struct tut1_physical_device *phy_dev
 			if (res)
 			{
 				printf("-- failed for device %u\n", i);
-				return;
+				goto exit_fail;
 			}
 		}
 	}
 
+exit_fail:
 	for (uint32_t i = 0; i < dev_count; ++i)
 		tut7_render_cleanup_essentials(&essentials[i], &devs[i]);
 }

tut8/main.c
@@ -427,7 +427,7 @@ static void render_loop(struct tut1_physical_device *phy_dev, struct tut2_device
 		/* See this function in tut7_render.c for explanations */
 		res = tut7_render_start(&essentials, dev, swapchain, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL, &image_index);
 		if (res)
-			return;
+			break;
 
 		/*
 		 * As a reminder, tut7_render_start() starts recording in our command buffer, and tut7_render_finish()
@@ -542,7 +542,7 @@ static void render_loop(struct tut1_physical_device *phy_dev, struct tut2_device
 		/* See this function in tut7_render.c for explanations */
 		res = tut7_render_finish(&essentials, dev, swapchain, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL, image_index);
 		if (res)
-			return;
+			break;
 	}
 
 exit_bad_render_data:

tut9/main.c
@@ -485,7 +485,8 @@ static void free_render_data(struct tut2_device *dev, struct tut7_render_essenti
 
 	tut8_free_pipelines(dev, &render_data->pipeline, 1);
 	tut8_free_layouts(dev, &render_data->layout, 1);
-	tut7_free_buffers(dev, render_data->buffers, 2);	/* Note: BUFFER_VERTICES_STAGING is already freed */
+	tut7_free_images(dev, render_data->images, 1);
+	tut7_free_buffers(dev, render_data->buffers, 3);
 	tut7_free_shaders(dev, render_data->shaders, 2);
 	tut7_free_graphics_buffers(dev, render_data->gbuffers, essentials->image_count, render_data->render_pass);
 
@@ -597,7 +598,7 @@ static void render_loop(struct tut1_physical_device *phy_dev, struct tut2_device
 		/* We saw all this in Tutorial 8.  Any changes are commented. */
 		res = tut7_render_start(&essentials, dev, swapchain, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL, &image_index);
 		if (res)
-			return;
+			break;
 
 		/* Render pass */
 		VkClearValue clear_values[2] = {
@@ -663,7 +664,7 @@ static void render_loop(struct tut1_physical_device *phy_dev, struct tut2_device
 
 		res = tut7_render_finish(&essentials, dev, swapchain, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL, image_index);
 		if (res)
-			return;
+			break;
 	}
 
 exit_bad_render_data:

commit c9d186fe12887addbb2133a9f50959fe93af87dc

Fix a bug introduced in 5f718746
Signed-off-by: Shahbaz Youssefi <ShabbyX@gmail.com>

tut6/main.c
@@ -115,7 +115,7 @@ static void render_loop(uint32_t dev_count, struct tut1_physical_device *phy_dev
 			if (supports)
 			{
 				present_queue[i] = devs[i].command_pools[j].queues[0];
-				goto exit_fail;
+				break;
 			}
 		}
 